be35f550befc25eac7d32d0f31e8b44be10ba7a4
Bernardo Ragonezi <bernardo_ragonezi@hotmail.com>
2021-12-06 19:58:05 -0300

    Create sub_ph.js

---
+++ sub_ph.js
@@ -0,0 +1,34 @@
+// MQTT subscriber
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://localhost:1234')
+var topic = 'sensor_ph'
+var mysql = require('mysql');
+
+
+client.on('message', (topic, message)=>{
+    message = message.toString()
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    let query = "INSERT INTO monitored_data.ph (ph, `data`) VALUES('"+message+"', '"+dateTime+"')"
+
+    //create connection
+    var con = mysql.createConnection({
+      host: "database-1.c6h2jt1jsshs.us-east-2.rds.amazonaws.com",
+      user: "admin",
+      password: "teste123",
+      database: "monitored_data"
+    });
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query(query, function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+        });
+      });
+})
+
+client.on('connect', ()=>{
+    client.subscribe(topic)
+})
